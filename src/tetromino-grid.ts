fbe39d10a0a3171b94eaa79a6f67341a4d914bc4
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-15 13:30:45 +0200

    getRandom method to create a random tetromino

--- src/tetromino-grid.ts
+++ src/tetromino-grid.ts
@@ -6,6 +6,15 @@ export interface Tetromino {
 
 export class TetrominoGrid {
 
+    getRandom() {
+        const random = Math.random();
+        if (random < 0.2) return this.tetromino_1;
+        else if (random < 0.4) return this.tetromino_2;
+        else if (random < 0.6) return this.tetromino_3;
+        else if (random < 0.8) return this.tetromino_4;
+        else return this.tetromino_5;
+    }
+
     get tetromino_1() {
         return [[
             [0, 2, 0, 0],
