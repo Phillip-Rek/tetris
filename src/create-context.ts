34a3b5dd5419a000f72ea79d9e8a3cadda439f3a
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-15 12:14:30 +0200

    feat: create context function to create canvas and context

---
+++ src/create-context.ts
@@ -0,0 +1,19 @@
+export function createContext(w?: number, h?: number) {
+
+    let canvas = document.createElement("canvas");
+
+    if (Array.from(document.getElementsByTagName("canvas")).length > 0) {
+        canvas = Array.from(document.getElementsByTagName("canvas"))[0];
+    }
+
+    document.body.appendChild(canvas);
+    canvas.width = w || 390;
+    canvas.height = h || 600;
+    canvas.style.backgroundColor = "#fff";
+    canvas.style.width = "390px";
+    canvas.style.height = "600px";
+    document.getElementById("draw-pad")?.appendChild(canvas);
+
+    const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+    return [ctx, canvas];
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import "font-awesome/css/font-awesome.min.css";
+import { createHTMLElement } from "./utils/utils";
 
 
+
+const gameContainer = createHTMLElement("div");
+
+function main() {
+    console.log(55)
+
+}
+
+main();
