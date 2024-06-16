e6c2cdbe201e147ed582cd3a3e7030970714e3e3
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-16 10:37:16 +0200

    score element to display the score

--- src/sidepanel/index.ts
+++ src/sidepanel/index.ts
@@ -1,7 +1,9 @@
 export class SidePanel {
+    container: HTMLElement;
+
     constructor() {
 
-        const sidePanel = this.createContainer();
+        this.container = this.createContainer();
 
     }
 
@@ -10,9 +12,24 @@ export class SidePanel {
         sidePanel.style.width = "350px";
         sidePanel.style.height = "100%";
         sidePanel.style.border = "2px solid red";
+        sidePanel.style.display = "flex";
+        sidePanel.style.flexDirection = "column"
 
         document.body.appendChild(sidePanel);
 
         return sidePanel;
+    }
+
+    createScoreElement() {
+        const scoreContainer = document.createElement("div");
+        const scoreElement = document.createElement("p");
+        scoreContainer.appendChild(scoreElement);
+
+        scoreContainer.style.width = "100%";
+        scoreContainer.style.border = "2px solid green";
+
+        this.container.appendChild(scoreContainer);
+
+        return scoreContainer;
     }
 }
