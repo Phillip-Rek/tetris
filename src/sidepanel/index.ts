8dab38682a83729043b81acc11853491ffb72399
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-16 11:05:47 +0200

    show instructions for playing the game

--- src/sidepanel/index.ts
+++ src/sidepanel/index.ts
@@ -6,14 +6,16 @@ export class SidePanel {
         this.container = this.createContainer();
         this.scoreElement = this.createScoreElement();
 
+        this.showInstructions();
     }
 
     createContainer() {
         const sidePanel = document.createElement("div");
-        sidePanel.style.width = "200px";
+        sidePanel.style.width = "250px";
         sidePanel.style.height = "600px";
-        sidePanel.style.border = "2px solid red";
+        sidePanel.style.border = "1px solid #00af";
         sidePanel.style.display = "flex";
+        sidePanel.style.padding = "0px 15px";
         sidePanel.style.flexDirection = "column"
 
         document.body.appendChild(sidePanel);
@@ -28,7 +30,7 @@ export class SidePanel {
         scoreContainer.appendChild(scoreElement);
 
         scoreContainer.style.width = "100%";
-        scoreContainer.style.border = "2px solid green";
+        // scoreContainer.style.border = "2px solid green";
 
         this.container.appendChild(scoreContainer);
 
@@ -37,5 +39,34 @@ export class SidePanel {
 
     updateScore(score: string) {
         this.scoreElement.innerHTML = "<b>Score: </b>" + score;
+    }
+
+    showInstructions() {
+        const container = document.createElement("div");
+
+        const instructions = document.createElement("p");
+        instructions.innerHTML = "<b>HOW TO PLAY</b>";
+
+        const left = document.createElement("p");
+        left.innerHTML = "<b>Left:</b> Left Arrow [<]";
+
+        const right = document.createElement("p");
+        right.innerHTML = "<b>Right:</b> Right Arrow [>]";
+
+        const down = document.createElement("p");
+        down.innerHTML = "<b>Down:</b> Down Arrow [v]";
+
+        const rotate = document.createElement("p");
+        rotate.innerHTML = "<b>Rotate:</b> Enter [<-]";
+
+        container.appendChild(instructions);
+        container.appendChild(left);
+        container.appendChild(right);
+        container.appendChild(down);
+        container.appendChild(rotate);
+
+        this.container.appendChild(container);
+
+        return container;
     }
 }
